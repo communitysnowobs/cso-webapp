3bc21fefb9dd3ceb094d653498680486af7cacdb
lsetiawan <landungs@uw.edu>
2017-05-18 12:25:18 -0700

    Create REST API for MountainHub Data

---
+++ apps/mtnhub/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MtnhubConfig(AppConfig):
+    name = 'mtnhub'
--- cso/settings/base.py
+++ cso/settings/base.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
+    'rest_framework',
+    'apps.mtnhub'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,15 @@ WSGI_APPLICATION = 'cso.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'cso',
+        'USER': 'lsetiawan',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'OPTIONS': {
+            'options': '-c search_path=admin,cso'
+        }
     }
 }
 
